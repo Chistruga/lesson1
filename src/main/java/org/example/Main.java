4b3dd0e2c0845ce7a1969282bd4cc4c09691fc3c
dchistruga <daniela.chistruga@endava.com>
2023-09-27 19:23:13 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.example.person.Person;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person();
+        System.out.println(person.age);
+    }
+}
---
+++ src/main/java/org/example/person/Person.java
@@ -0,0 +1,11 @@
+package org.example.person;
+
+public class Person {
+    public int age = 20;
+
+    public int getAge() {
+        return age;
+    }
+
+
+}
